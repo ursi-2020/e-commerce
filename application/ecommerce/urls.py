e67bb159970c195a0b98ae463fb56577e7e24442
Baptiste <baptiste@saintandr.com>
2019-09-27 14:08:30 +0200

    Nice display for products

--- application/ecommerce/urls.py
+++ application/ecommerce/urls.py
@@ -4,13 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('hello', views.helloWolrd, name='hello'),
-    #path('print', views.printJson, name='print'),
-    #path('ihm', views.displayForm, name='field'),
-    #path('search', views.search, name='search'),
-    #path('save', views.saveDB, name='save'),
-    path('read', views.readDB, name='read'),
-    #path('info', views.getInfo, name='info'),
     path('products', views.displayProducts, name='products'),
     path('remove', views.removeDB, name='remove'),
     path('connect', views.connect, name="connect")
